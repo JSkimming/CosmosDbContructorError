4aaa581c869243b1900323873408c95b4c48dd89
James Skimming <JSkimming@users.noreply.github.com>
2017-07-14 14:22:47 +0100

    Added Create a DocumentClient using version 1.11.4
    It looks like Version 1.11.4 is the same version of the NuGet package
    that is used with Azure.Functions.Cli 1.0.0-beta.100

--- CosmosDbError/CreateClient.cs
+++ CosmosDbError/CreateClient.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.Documents.Client;
 
 namespace CosmosDbError
 {
@@ -10,6 +11,15 @@ namespace CosmosDbError
         public static void Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, TraceWriter log)
         {
             log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
+            var connectionPolicy = new ConnectionPolicy
+            {
+                UserAgentSuffix = ".CosmosDbError",
+            };
+
+            var client = new DocumentClient(
+                new Uri("https://localhost:8081/"),
+                "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
+                connectionPolicy);
         }
     }
 }
